793f865d27412f4e033b129da6a3c1f155704307
Natalia <nat.kramarz@gmail.com>
2020-10-11 11:36:47 +0200

    added tests

--- insertionSort/main.cpp
+++ insertionSort/main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <random>
 #include "InsertionSort.cpp"
 
 int main() {
     InsertionSort sort = InsertionSort();
-    std::vector<int> array { 12, 11, 8, 5, 6 };
-    sort.array = array;
-    sort.sort();
-    for (int i = 0; i < 5; i++) {
-        std::cout << sort.array[i] << " ";
+    std::default_random_engine sizeGenerator;
+    std::uniform_int_distribution<int> sizeDistribution(1, 200);
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(-1000, 1000);
+    for(int j = 0; j < 20; j++) {
+        int size = sizeDistribution(sizeGenerator);
+        std::vector<int> array(size);
+        for (int i = 0; i < size; i++) {
+            array[i] = distribution(generator);
+        }
+        sort.array = array;
+        sort.sort();
+        for (int i = 0; i < size; i++) {
+            std::cout << sort.array[i] << ", ";
+        }
+        std::cout << std::endl;
+        std::cout << "_____________________________________";
+        std::cout << std::endl;
     }
     return 0;
 }
