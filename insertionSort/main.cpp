31e2ea888373e844c89c8c5710b0488e1cad168e
Natalia <nat.kramarz@gmail.com>
2020-10-11 11:58:02 +0200

    minor tests changes

--- insertionSort/main.cpp
+++ insertionSort/main.cpp
@@ -4,10 +4,12 @@
 
 int main() {
     InsertionSort sort = InsertionSort();
+
     std::default_random_engine sizeGenerator;
     std::uniform_int_distribution<int> sizeDistribution(1, 200);
     std::default_random_engine generator;
     std::uniform_int_distribution<int> distribution(-1000, 1000);
+
     for(int j = 0; j < 20; j++) {
         int size = sizeDistribution(sizeGenerator);
         std::vector<int> array(size);
@@ -18,5 +20,6 @@ int main() {
         sort.sort();
         sort.displayArray();
     }
+
     return 0;
 }
