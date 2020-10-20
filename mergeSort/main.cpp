1cd8b773c854f9e7a1b1a1387a362c0956bf135e
Natalia <nat.kramarz@gmail.com>
2020-10-20 16:21:03 +0200

    Added tests

--- mergeSort/main.cpp
+++ mergeSort/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include "mergeSort.cpp"
 #include <vector>
+#include <random>
+
 int main() {
     std::vector<int> array{1, 2, 3, 2, 4};
     MergeSort sort = MergeSort();
-    sort.array = array;
-    sort.sort(0, array.size() -1 );
-    sort.displayArray();
+    std::default_random_engine sizeGenerator;
+    std::uniform_int_distribution<int> sizeDistribution(1, 200);
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(-1000, 1000);
+
+    for(int j = 0; j < 20; j++) {
+        int size = sizeDistribution(sizeGenerator);
+        std::vector<int> array(size);
+        for (int i = 0; i < size; i++) {
+            array[i] = distribution(generator);
+        }
+        sort.array = array;
+        sort.sort(0, array.size() - 1);
+        sort.displayArray();
+    }
     return 0;
 }
