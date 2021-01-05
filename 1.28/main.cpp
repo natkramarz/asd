49d39d81f88beebba4ea1ecc6d63c615fd278d88
Natalia <nat.kramarz@gmail.com>
2021-01-05 23:42:26 +0100

    added destructor

--- 1.28/main.cpp
+++ 1.28/main.cpp
@@ -3,47 +3,51 @@
 // S is a subset of set {1, 2, ..., n}
 // an array is not initialized, so there can be garbage values in it
 
-
-
 class S{
-        int * set;
-        int * from;
-        int * to;
-        int n;
-        int top;
+    int * set;
+    int * from;
+    int * to;
+    int n;
+    int top;
 public:
-        S(int size){
-            n = size;
-            set = new int [size + 1];
-            from = new int[size + 1];
-            to = new int[size + 1];
-            top = 0;
-        }
+    S(int size){
+        n = size;
+        set = new int [size + 1];
+        from = new int[size + 1];
+        to = new int[size + 1];
+        top = 0;
+    }
 
-        int select(){
-            top--;
-            int i = to[top];
-            std::cout << i << std::endl;
-            from[i] = 0;
-            to[top] = 0;
-        }
+    int select(){
+        top--;
+        int i = to[top];
+        std::cout << i << std::endl;
+        from[i] = 0;
+        to[top] = 0;
+    }
 
-        bool search(int i){
-            if(i < 1 || i > n) return false;
-            if(from[i] < top && to[from[i]] == i){
-                return true;
-            }
-            return false;
+    bool search(int i){
+        if(i < 1 || i > n) return false;
+        if(from[i] < top && to[from[i]] == i){
+            return true;
         }
+        return false;
+    }
 
-        void insert(int i){
-            if(i < 1 || i > n) return;
-            set[i] = 1;
-            from[i] = top;
-            to[top] = i;
-            if(top < n)
-                top++;
-        }
+    void insert(int i){
+        if(i < 1 || i > n) return;
+        set[i] = 1;
+        from[i] = top;
+        to[top] = i;
+        if(top < n)
+            top++;
+    }
+
+    ~S(){
+        delete [] set;
+        delete [] from;
+        delete [] to;
+    }
 };
 
 
